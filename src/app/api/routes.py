3ce079afad6a780a6d4548c4f7b24596b7673900
pplayer1712 <p.player1712.ad@gmail.com>
2025-05-16 21:26:00 +0200

    added endpoint to routes.py

--- src/app/api/routes.py
+++ src/app/api/routes.py
@@ -115,12 +115,12 @@ def change_contact():
     return jsonify({"success": "Contact status changed successfully"}), 200
 
 
-@api_bp.route("/deleteContact", methods=['POST'])
+@api_bp.route("/changeProfile", methods=['POST'])
 @jwt_required()
 def change_profile():
     user_id = get_jwt_identity()
     data = request.json if request.is_json else request.args
-
+    new_value = data.get('new_value')
     action = data.get('action')
 
     if not action:
@@ -128,7 +128,7 @@ def change_profile():
     if action not in ["picture", "name", "password"]:
         return jsonify({"error": "'action' must be either 'picture', 'name' or 'password'"}), 400
     if action == "picture":
-        new_value = data.get('new_value')
+        
         if not new_value:
             return jsonify({"error": "'new_value' is required"}), 400
         result = user_service.change_profile_picture(user_id, new_value)
@@ -141,7 +141,10 @@ def change_profile():
         new_value = data.get('new_value')
         if not new_value:
             return jsonify({"error": "'new_value' is required"}), 400
-        result = user_service.change_password(user_id, new_value)
+        old_password = data.get('old_password')
+        if not old_password:
+            return jsonify({"error": "'old_password' is required"}), 400
+        result = user_service.change_password(user_id, old_password, new_value)
 
 
 @api_bp.route("/getChats", methods=['GET'])
